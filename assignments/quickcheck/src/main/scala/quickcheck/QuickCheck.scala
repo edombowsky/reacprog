8f9feb76079878ffd2609af622476f22788b568c
Earl Dombowsky <earl.dombowsky@ventyx.abb.com>
2013-11-29 15:42:38 -0800

    Final submission

--- assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -8,14 +8,69 @@ import Gen._
 import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
-
   property("min1") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
 
-  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+  property("Min works inside the heap") =
+    forAll { (a: Int, b: Int) =>
+      (a min b) == findMin(insert(b, insert(a, empty)))
+    }
 
+  property("Melded's min should be the min of left or right") =
+    forAll { (l: H, r: H) =>
+      val melded = meld(l, r)
+      if (isEmpty(l) && isEmpty(r))
+        isEmpty(melded)
+      else if (isEmpty(l))
+        findMin(melded) == findMin(r)
+      else if (isEmpty(r))
+        findMin(melded) == findMin(l)
+      else {
+        val meldedMin = findMin(melded)
+        meldedMin == (findMin(l) min findMin(r))
+      }
+    }
+
+  property("insert and delete should equal empty.") =
+    forAll { i: Int => isEmpty(deleteMin(insert(i, empty))) }
+
+  protected def popUntilEmpty(h: H): List[A] =
+    if (isEmpty(h))
+      List.empty
+    else
+      findMin(h) :: popUntilEmpty(deleteMin(h))
+
+  property("Popping should yield a sorted list.") =
+    forAll { h: H =>
+      val items = popUntilEmpty(h)
+      items.sorted == items
+    }
+
+  property("Melding two heaps should contain all elements from each of the two component heaps.") =
+    forAll { (l: H, r: H) =>
+      popUntilEmpty(meld(l, r)).sorted ==
+        (popUntilEmpty(l) ++ popUntilEmpty(r)).sorted
+    }
+
+  /**
+   * Generator for arbitrary heaps.
+   *
+   * @type {[type]}
+   */
+  lazy val genHeap: Gen[H] = for {
+    i <- arbitrary[Int]
+    h <- oneOf(value(empty), genHeap)
+  } yield insert(i, h)
+
+  lazy val emptyHeap: Gen[H] = value(empty)
+
+  implicit lazy val arbHeap: Arbitrary[H] =
+    Arbitrary(oneOf(genHeap, emptyHeap))
 }
