1650c50a6d535968de92cf174f7ae9395818dfb6
Earl Dombowsky <earl.dombowsky@ventyx.abb.com>
2013-12-03 14:38:36 -0800

    Implement textValues

--- assignments/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ assignments/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -10,6 +10,9 @@ import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
 import rx.lang.scala.Observable
+import rx.lang.scala._
+import rx.lang.scala.subscriptions._
+import rx.lang.scala.subjects._
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,7 +54,14 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
+    def textValues: Observable[String] = {
+      val subject = PublishSubject[String]("")
+      field subscribe {
+        case ValueChanged(textValue) => subject.onNext(textValue.text)
+      }
+
+      subject
+    }
 
   }
 
